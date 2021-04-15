ef136a8884b55e9053d86f0410caacd2408cdcb7
Jason <codethejason@outlook.com>
2021-04-14 23:38:11 -0400

    added more knight tests for complete branch coverage

--- src/test/java/pieces/KnightTest.java
+++ src/test/java/pieces/KnightTest.java
@@ -2,11 +2,8 @@ package pieces;
 
 import chess.Cell;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -32,22 +29,32 @@ public class KnightTest {
     @Test
     public void KnightMove() {
         Set<Cell> expected = new HashSet<>();
-        expected.add(board[0][2]);
-        expected.add(board[2][2]);
-        expected.add(board[3][1]);
-        Set<Cell> result = new HashSet<>(knight.move(board, 1, 0));
+        expected.add(board[6][5]);
+        expected.add(board[5][6]);
+        Set<Cell> result = new HashSet<>(knight.move(board, 7, 7));
         assertEquals(expected, result);
     }
 
     @Test
     public void KnightMoveBlockedByOwnPiece() {
-        Knight knight2 = new Knight("WK02","/White_Knight.png",0);
-        Cell c = new Cell(1, 2, knight2);
-        board[1][2] = c;
+		Pawn pawn1 = new Pawn("WP01","/White_Pawn.png",0);
+		Pawn pawn2 = new Pawn("BP01","/Black_Pawn.png",1);
+        Cell c1 = new Cell(1, 2, pawn1);
+        Cell c2 = new Cell(2, 1, pawn2);
+        board[1][2] = c1;
+        board[2][1] = c2;
+        board[2][1] = c2;
         
         Set<Cell> expected = new HashSet<>();
         expected.add(board[2][1]);
         Set<Cell> result = new HashSet<>(knight.move(board, 0, 0));
         assertEquals(expected, result);
     }
+
+    // @Test
+    // public void KnightTestInvalidMove() {
+    //     Set<Cell> result = new HashSet<>(knight.move(board, -1, -1));
+    //     assertEquals(new HashSet<Cell>(), result);
+    //     // Should return empty
+    // }
 }
